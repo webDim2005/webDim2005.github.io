6c79eea67289ab8be5f3cb4752edefcec8930c20
webDim2005 <95935406+webDim2005@users.noreply.github.com>
2022-01-30 16:16:23 +0300

    Delete ZNY directory

--- ZNY/js/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('.menu');
-let burger = document.querySelector('.burger');
-let body = document.querySelector('body');
-burger.onclick = function(){
-  burger.classList.toggle("active");
-  menu.classList.toggle("active");
-  body.classList.toggle("active");
-}
